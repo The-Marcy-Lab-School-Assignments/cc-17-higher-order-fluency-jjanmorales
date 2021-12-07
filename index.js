28c06e9fb2e91dc79687b0bcdca0f348dd4a51a4
Jan Morales <moralesjan40@gmail.com>
2021-12-07 15:25:23 +0000

    FINISHED

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //write your code here
+const pluralize = words => words.map(word => word + 's');
+
+const animals = ['cheetah','dog', 'cat', 'dodobird', 'bear', 'dolphin']
+console.log(pluralize(animals)) // 
+
+const upperCase = words => words.map(word => word[0].toUpperCase() + word.substr(1, word.length));
+console.log(upperCase(animals));
+
+const longWords = words => words.filter(word => word.length > 3);
+console.log(longWords(animals));
+
+const shortWords = words => words.filter(word=> word.length < 5);
+console.log(shortWords(animals));
+
+const oddLength = words => words.filter(word => word.length % 2 !== 0);
+console.log(oddLength(animals));
+
+const longToShort = words => words.sort((a,b) => b.length - a.length);
+console.log(longToShort(animals));
+
+const numbers = [22, 15, 1114, 416, 37, 4]
+const sum = words => words.reduce((a,b) => a + b);
+console.log(sum(numbers));
+
